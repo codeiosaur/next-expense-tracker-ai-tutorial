4973aca869ca55cf4a8082953f2978c4bc43776b
codeiosaur <codeiosaur@outlook.com>
2025-10-02 21:29:04 +0200

    Completed Navbar component and guest/about/contact pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
-export default function HomePage() {
+import { currentUser } from '@clerk/nextjs/server'
+import Guest from '@/components/Guest'
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
+
   return (
     <div className='text-red-400'>HomePage</div>
   );
